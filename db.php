79db0d78c10213008ff6eaa862089434dbc16b8f
Hartmodz <98187816+hartmodz@users.noreply.github.com>
2022-07-15 20:06:38 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'git');
+define('DB_NAME', 'nombre de su db');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 if (mysqli_connect_errno()) {
     echo "Error mysql" . mysqli_connect_error();
